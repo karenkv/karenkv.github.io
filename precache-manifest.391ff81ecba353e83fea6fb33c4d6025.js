79cd0353bcc4381278440b687d08d59250855e63
Karen Vu <karenkv@uci.edu>
2020-08-10 17:11:12 -0700

    Updates

---
+++ precache-manifest.391ff81ecba353e83fea6fb33c4d6025.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242445ed95cffe886c804e45f9984486",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cc66e006abdf8bdb429",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c0ae9bc34172c4e49d9",
+    "url": "/static/js/2.6341bbdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6341bbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cc66e006abdf8bdb429",
+    "url": "/static/js/main.90d303d8.chunk.js"
+  },
+  {
+    "revision": "5f2a348dc275c090ccc8",
+    "url": "/static/js/runtime-main.69c1266b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
