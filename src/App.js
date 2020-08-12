2e9912b9c23c26707672205940bc266dda248ca4
Karen Vu <karenkv@uci.edu>
2020-08-12 16:29:59 -0700

    added dark mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import headshot from './assets/karen-vu-headshot.JPG';
+import React, {useState} from 'react';
+import headshot from './assets/karen-vu-headshot.jpeg';
 import github from './assets/github.png';
 import devpost from './assets/devpost.png';
 import linkedin from './assets/linkedin.png';
@@ -7,16 +7,29 @@ import resume from './assets/resume.png';
 import pdf from './assets/resume.pdf';
 import './App.css';
 
-function App() {
+const App = () => {
+    const [theme, setTheme] = useState("light");
+
+    const handleThemeChange = () => {
+        theme === "light" ? setTheme("dark") : setTheme("light");
+    }
+
     return (
-        <div className={"App "}>
+        <div className={"App " + theme}>
+            <div className="theme">
+                <label className="switch">
+                    <input type="checkbox" onChange={handleThemeChange}
+                           checked={theme === "dark"}/>
+                    <span className={"slider " + {theme}}/>
+                </label>
+            </div>
             <div className="header">
                 <div className="headshot">
                     <img alt="Karen Vu's headshot" src={headshot}/>
                 </div>
                 <h1>Hello, World! My name is Karen Vu</h1>
             </div>
-            <div className="links">
+            <div className={"links " + theme}>
                 <a href="https://github.com/karenkv" target="_blank" rel="noopener noreferrer">
                     <img alt="Github Icon" src={github} width="32px" height="32px"/>
                 </a>
