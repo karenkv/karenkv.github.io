3c34e50f62210c992f8b5c32180d16ad0a027cbd
Karen Vu <karenkv@uci.edu>
2020-08-12 11:12:35 -0700

    adjusting image size

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
                 <div className="headshot">
                     <img alt="Karen Vu's headshot" src={headshot}/>
                 </div>
-                <h1>Hello World! My name is Karen Vu</h1>
+                <h1>Hello, World! My name is Karen Vu</h1>
             </div>
         </div>
     );
