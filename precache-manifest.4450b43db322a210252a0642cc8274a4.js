020ea57ef05495274bb5c5967021ffa3d1c19618
Karen Vu <karenkv@uci.edu>
2020-08-12 10:47:35 -0700

    Updates

---
+++ precache-manifest.4450b43db322a210252a0642cc8274a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e89beb638f6e45f996ca86d96fa2d89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "160b3eefc2cf3c1f082a",
+    "url": "/static/css/main.7773fd58.chunk.css"
+  },
+  {
+    "revision": "4c0ae9bc34172c4e49d9",
+    "url": "/static/js/2.6341bbdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6341bbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "160b3eefc2cf3c1f082a",
+    "url": "/static/js/main.5a7f5abc.chunk.js"
+  },
+  {
+    "revision": "5f2a348dc275c090ccc8",
+    "url": "/static/js/runtime-main.69c1266b.js"
+  },
+  {
+    "revision": "30e4ca74f5082585899fadb7d1dd71a1",
+    "url": "/static/media/karen-vu-headshot.30e4ca74.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.096b592074def78ceeaf056ec42ba2d8.js"
+  "/precache-manifest.4450b43db322a210252a0642cc8274a4.js"
 );
 
 self.addEventListener('message', (event) => {
