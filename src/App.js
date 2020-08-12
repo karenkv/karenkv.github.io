75d30fcf15718c041b0740c0da0ee5aa3d57dd64
Karen Vu <karenkv@uci.edu>
2020-08-12 10:51:00 -0700

    fixing image size and placement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import headshot from './assets/karen-vu-headshot.JPG';
 import './App.css';
-import Header from "./components/Header";
 
 function App() {
     return (
         <div className={"App "}>
-            <Header/>
+            <div className="header">
+                <div className="headshot">
+                    <img alt="Karen Vu's headshot" src={headshot}/>
+                </div>
+                <h1>Hello World! My name is Karen Vu</h1>
+            </div>
         </div>
     );
 }
